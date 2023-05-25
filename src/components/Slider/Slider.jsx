6d9504d9055e4f7efaabe77c3687830d8fb1655f
Igor Trifonov <yorherim@gmail.com>
2023-05-25 09:21:48 +0800

    [feat]: add slider

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,33 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { A11y, Autoplay, Navigation, Pagination } from "swiper";
+import "swiper/swiper-bundle.min.css";
+
+import styles from "./Slider.module.css";
+
+export const Slider = ({ pictures, className }) => {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, A11y, Autoplay]}
+      spaceBetween={50}
+      slidesPerView={1}
+      className={`${styles.slider} ${className}`}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      navigation
+      pagination={{ clickable: true }}
+      loop={true}
+    >
+      {pictures.map((picture) => {
+        return (
+          <SwiperSlide key={picture.id}>
+            <div className={styles["img-container"]}>
+              <img src={picture.address} alt="picture" className={styles.img} />
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
